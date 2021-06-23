d45b4737ed461421dc19adfca73900f93cc88f27
Ewoud Kuijpers <74767424+Ewaudtkuijpers@users.noreply.github.com>
2021-06-23 10:09:58 +0000

    De Computer kiest random 4 kleuren

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+import os
+
+print("hallo je gaat nu mastermind spelen")
+kleuren = ["Rood", "Geel", "Groen", "Blauw", "Zwart", "Oranje"]
+
+random.shuffle(kleuren)
+passcode = kleuren[:4]
+
+print(passcode)
