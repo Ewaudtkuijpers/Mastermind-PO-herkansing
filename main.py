ce2cf0e00b3693dacaa1d49b24a434537851a59a
Ewoud Kuijpers <74767424+Ewaudtkuijpers@users.noreply.github.com>
2021-06-24 15:14:24 +0000

    Speler ziet of de kleur in de passcode zit

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ import random
 import os
 
 print("hallo je gaat nu mastermind spelen")
+
+
 kleuren = ["Rood", "Geel", "Groen", "Blauw", "Zwart", "Oranje"]
 
 random.shuffle(kleuren)
 passcode = kleuren[:4]
 
-input("\tvoer hier je code in:")
+def mastermind():
+  invoer = input("\nvoer hier je code in:")
+  if invoer in passcode:
+    print("Wit")
 
+mastermind()
