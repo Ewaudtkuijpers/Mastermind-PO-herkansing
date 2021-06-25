29b177926d55f5ddd5a56c8538deef0b9ab159be
Ewoud Kuijpers <74767424+Ewaudtkuijpers@users.noreply.github.com>
2021-06-25 08:52:18 +0000

    Vergelijken of de invoer erin zit, op de goede plek zit of er helmaal niet in zit

--- main.py
+++ main.py
@@ -11,11 +11,20 @@ passcode = kleuren[:4]
 print(passcode)
 def mastermind():
   invoer = input("\nvoer hier je code in:")
-  for letter in invoer:
-    if letter in passcode:
-      print(letter + " zit erin")
+  for i in range(4):
+    letterCode = passcode[i]
+    letterInvoer = invoer[i]
+    print("letter " + letterCode + " vergelijken met " +letterInvoer);
+    if letterCode == letterInvoer:
+      print("letter op goede plek")
+    elif letterInvoer in passcode:
+      print("letter op de verkeerde plek")
     else:
-      print(letter + " zit er niet in")
+      print("letter zit er niet in")
+    # if letter in passcode:
+    #   print(letter + " zit erin")
+    # else:
+    #   print(letter + " zit er niet in")
   
 
 mastermind()
