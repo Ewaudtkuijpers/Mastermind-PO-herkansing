2cfc9a047b1a17d9a4b036d00bfaf10efa72387b
Tijmen Schouten <74767479+tijmenhero@users.noreply.github.com>
2021-06-25 14:55:54 +0000

    alleen maar letters invoeren

--- main.py
+++ main.py
@@ -11,16 +11,25 @@ passcode = kleuren[:4]
 print(passcode)
 def mastermind():
   hints = []
+
+  #invoer
   invoer = input("\nvoer hier je code in:").upper()
-  for i in range(4):
-    letterCode = passcode[i]
-    letterInvoer = invoer[i]
-    print("letter " + letterCode + " vergelijken met " +letterInvoer);
-    if letterCode == letterInvoer:
-      hints.append("z")
-    elif letterInvoer in passcode:
-      hints.append("w")
-  print(hints)
+  if not invoer.isalpha():
+    print("invoer fout probeer opnieuw")
+    
+
+  #Code vergelijken met invoer en hints geven
+    for i in range(4):
+      letterCode = passcode[i]
+      letterInvoer = invoer[i]
+      print("letter " + letterCode + " vergelijken met " +letterInvoer);
+      if letterCode == letterInvoer:
+        hints.append("z")
+      elif letterInvoer in passcode:
+        hints.append("w")
+    print(hints)
+  
+  # checken of je wint
   if invoer == "".join(passcode):
     print("je hebt gewonnen")
   else:
